b8fdda16d6d67cd67445f60182406b3b52ec43a3
ppajot <ppajot@student.42.fr>
2024-02-28 17:41:00 +0100

    describe

--- describe.py
+++ describe.py
@@ -3,6 +3,7 @@
 import sys
 import csv
 import numpy as np
+import math
 
 def open_file(file):
 	file_array = []
@@ -17,12 +18,99 @@ def open_file(file):
 	data_array = np.array(file_array)
 	return np.transpose(data_array)
 
+def sort(array):
+	size = array.size
+	for i in range (0, size - 1):
+		for j in range (0, size - 1 - i):
+			if (array[j] > array[j + 1]):
+				array[j], array[j + 1] = array[j + 1], array[j]
+
+def count_non_zero(array):
+	count = 0
+	for elem in array:
+		if elem != 0:
+			count += 1
+	return count
+
+def del_zeros(array):
+	# print(array)
+	for i in range (0, array.size):
+		if array[i] == 0:
+			np.delete(array, i)
+
+def get_percentile(sorted_array, perc):
+	size = sorted_array.size
+	if perc < 0:
+		return sorted_array[0]
+	if perc >= 100:
+		return sorted_array[size - 1]
+	return sorted_array[math.floor(size * perc / 100)]
+
 try:
 	open_file(sys.argv[1])
 except IndexError:
 	print("Usage: describe.py filename")
 	sys.exit(1)
 
-data = open_file(sys.argv[1])
-print(data[6])
-print(data[18])
+data_str = open_file(sys.argv[1])
+
+data_str[data_str==''] = '0'
+
+data_float = np.array(data_str[6:, 1:], float)
+# data_float[data_float==0] = None
+
+
+# print(data_str)
+# print(data_float)
+
+
+stats = np.empty((13, 9), float)
+
+index = 0
+for row in data_float:
+
+	del_zeros(row)
+	sort(row)
+	count = count_non_zero(row)
+	mean = sum(row) / count
+	var = sum((row - mean) ** 2) / count
+	std_dev = math.sqrt(var)
+	min = row[0]
+	max = row[count - 1]
+	perc_25 = get_percentile(row, 25)
+	median = get_percentile(row, 50)
+	perc_75 = get_percentile(row, 75)
+	stats[index][0] = count
+	stats[index][1] = mean
+	stats[index][2] = var
+	stats[index][3] = std_dev
+	stats[index][4] = min
+	stats[index][5] = perc_25
+	stats[index][6] = median
+	stats[index][7] = perc_75
+	stats[index][8] = max
+	# print(stats[index])
+	index += 1
+
+display = []
+display.append(" " * 10)
+display.append('{:10}'.format("Count"))
+display.append('{:10}'.format("Mean"))
+display.append('{:10}'.format("Var"))
+display.append('{:10}'.format("Std"))
+display.append('{:10}'.format("Min"))
+display.append('{:10}'.format("25%"))
+display.append('{:10}'.format("Median"))
+display.append('{:10}'.format("75%"))
+display.append('{:10}'.format("Max"))
+
+for i in range (0, 13):
+	display[0] += " | " +  '{:>13.13}'.format(data_str[6 + i][0])
+
+for i in range (0, 9):
+	for j in range (0, 13):
+		display[1 + i] +=  " | " +  '{:13.3f}'.format(stats[j][i])
+
+for row in display:
+	print(row)
+# print('{:10}'.format("count"), '{:15f}'.format(stats[0][0]))
